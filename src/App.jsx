f4285885f71e3dbdbcb02579472911dce74b6888
youjin <youjinyeo12@gmail.com>
2025-11-20 18:34:47 +0900

    ✨TanStack Query사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,38 @@
 import { Routes, Route } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+//import { useState, useEffect } from 'react'
+import { useQuery } from '@tanstack/react-query'
 import SongList from './pages/SongList.jsx'
 import SongDetail from './pages/SongDetail.jsx'
 import { getSongList } from './api/songApi'
 
-
 function App() {
-  const [songs, setSongs] = useState([])
+  // const [songs, setSongs] = useState([])
 
-  useEffect(() => {
-    const fetchSongs = async () => {
-      try {
-        const data = await getSongList()
-        setSongs(data)
-      } catch (err) {
-        console.error("Failed to fetch songs:", err)
-      }
-    }
+  // useEffect(() => {
+  //   const fetchSongs = async () => {
+  //     try {
+  //       const data = await getSongList()
+  //       setSongs(data)
+  //     } catch (err) {
+  //       console.error("Failed to fetch songs:", err)
+  //     }
+  //   }
 
-    fetchSongs()
-  }, [])
+  //   fetchSongs()
+  // }, [])
+
+  const { data: songs, isLoading, isError, error } = useQuery({
+    queryKey: ['songs'],
+    queryFn: getSongList
+  })
+
+  if (isLoading) {
+    return <p className="text-center mt-10">Loading...</p>
+  }
+
+  if (isError) {
+    return <p className="text-center mt-10">오류 발생: {error.message}</p>
+  }
 
   return (
     <Routes>
